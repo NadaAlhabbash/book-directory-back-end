d9ad240250ccfeb17b2204126afe97d470998acf
NadaAlhabbash <nadaalhabbash@gmail.com>
2022-08-23 13:24:46 +0300

    final edits before db

--- src/Controllers/bookAction/oneBook.js
+++ src/Controllers/bookAction/oneBook.js
@@ -1,11 +1,23 @@
 const { readJson } = require("../../Database/JSON");
+const queries = require ('../../Database/queries');
+const pool = require('../../../db');
 
 const oneBook = (req, res, data) => {
-  const books = JSON.parse(data);
-  const book = books.find((c) => c.id === parseInt(req.params.id));
-  if (!book) res.status(404).send("This book id is not found");
-  res.status(200, { "Content-Type": "application/json" }).send(book);
+//   const books = JSON.parse(data);
+//   const book = books.find((c) => c.id === parseInt(req.params.id));
+//   if (!book || book.deleted !== 'false'){
+//     res.status(404).send("This book id is not found");
+//   } else{
+//     res.status(200, { "Content-Type": "application/json" }).send(book);
+//   }
+const id = parseInt(req.params.id);
+  pool.query(queries.oneBook, [id], (err, data) => {
+    if (err) throw err;
+    console.log(data.rows);
+    res.status(200).json(data.rows);
+  });
 };
+
 
 const oneBookcb = (req, res) => {
   readJson(req, res, oneBook);
--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const web = require("./Routes/routes");
+require('dotenv').config();
 
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.use(web);
 
@@ -10,7 +12,7 @@ app.get("/", (req, res) => {
   res.send("Book Directory");
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
